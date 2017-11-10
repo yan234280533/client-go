534edd775f503851b45a6d5520f4c0ccb60b38ff
devinyan <devinyan@tencent.com>
2017-11-10 18:10:55 +0800

    中间结果

--- examples/create-from-file/main.go
+++ examples/create-from-file/main.go
@@ -21,138 +21,29 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"io"
 	"io/ioutil"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	v1beta1 "k8s.io/api/apps/v1beta1"
 )
 
-
-
-/*func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
+func PrintObjectType(obj runtime.Object) (error){
+	switch typed := obj.(type) {
+	case *v1beta1.Deployment:
+		fmt.Printf("Kind is v1beta1.Deployment\n")
+		fmt.Printf("typed:%s",typed.Kind)
+		return nil
+	default:
+		fmt.Printf("Unsupported kind when set object type: %v\n", obj)
+		return fmt.Errorf("Unsupported kind when set object type\n")
 	}
 
-	serviceClient := clientset.Core().Services(apiv1.NamespaceDefault)
-
-	servicePort := apiv1.ServicePort{
-			Protocol: "TCP",
-			Port:     int32(80),
-			NodePort: int32(0),
-			Name:     "tst",
-			TargetPort: intstr.IntOrString{
-				Type:   intstr.Int,
-				IntVal: int32(80),
-			},
-		}
-
-	service := &apiv1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "nginx",
-		},
-		Spec: apiv1.ServiceSpec{
-			Ports:[]apiv1.ServicePort{servicePort},
-		},
-	}
-
-	// Create Service
-	fmt.Println("Creating service...")
-	result, err := serviceClient.Create(service)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Created service %q.\n", result.GetObjectMeta().GetName())
-
-	result.Kind = "Service"
-	result.APIVersion = "v1"
-
-	textStr,err := PrintYamlObj(result)
-		if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Convert service:\n%s", textStr)
-
-	// Update Deployment
-	prompt()
-	fmt.Println("Updating service...")
-	//    You have two options to Update() this Deployment:
-	//
-	//    1. Modify the "deployment" variable and call: Update(deployment).
-	//       This works like the "kubectl replace" command and it overwrites/loses changes
-	//       made by other clients between you Create() and Update() the object.
-	//    2. Modify the "result" returned by Get() and retry Update(result) until
-	//       you no longer get a conflict error. This way, you can preserve changes made
-	//       by other clients between Create() and Update(). This is implemented below
-	//			 using the retry utility package included with client-go. (RECOMMENDED)
-	//
-	// More Info:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#concurrency-control-and-consistency
-
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		// Retrieve the latest version of Deployment before attempting update
-		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-		result, getErr := serviceClient.Get("nginx", metav1.GetOptions{})
-		if getErr != nil {
-			panic(fmt.Errorf("Failed to get latest version of service: %v", getErr))
-		}
-
-		result.Spec.Ports[0].Port = 81
-		_, updateErr :=serviceClient.Update(result)
-		return updateErr
-	})
-	if retryErr != nil {
-		panic(fmt.Errorf("Update failed: %v", retryErr))
-	}
-	fmt.Println("Updated service...")
-
-	// List service
-	prompt()
-	fmt.Printf("Listing service in namespace %q:\n", apiv1.NamespaceDefault)
-	list, err := serviceClient.List(metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-	for _, d := range list.Items {
-		fmt.Printf(" * %s (%s clusterIP)\n", d.Name, d.Spec.ClusterIP)
-	}
-
-	// Delete service
-	prompt()
-	fmt.Println("Deleting service...")
-	deletePolicy := metav1.DeletePropagationForeground
-	if err := serviceClient.Delete("nginx", &metav1.DeleteOptions{
-		PropagationPolicy: &deletePolicy,
-	}); err != nil {
-		panic(err)
-	}
-	fmt.Println("Deleted service.")
+	return nil
 }
-
-func prompt() {
-	fmt.Printf("-> Press Return key to continue.")
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		break
-	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-	fmt.Println()
-}
-
-func int32Ptr(i int32) *int32 { return &i }*/
 
 func read3(path string) string {
     fi, err := os.Open(path)
@@ -172,7 +63,43 @@ func main() {
 	str := read3(file)
 	t3 := time.Now()
 	fmt.Printf("Cost time %v\n", t3.Sub(start))
-	fmt.Printf("str:%s", str)
-	fmt.Printf("------------------------------------------------")
+	fmt.Printf("str:%s\n", str)
+	fmt.Printf("------------------------------------------------\n")
 
+	reader := strings.NewReader(str)
+	d := yaml.NewYAMLOrJSONDecoder(reader, 4096)
+
+        for {
+		data := unstructured.Unstructured{}
+		if err := d.Decode(&data); err != nil {
+			if err == io.EOF {
+				fmt.Printf("decode is over\n")
+				break
+			}
+			fmt.Printf("decode err:%s\n",err.Error())
+			break
+		}
+
+		version := data.GetAPIVersion()
+		kind := data.GetKind()
+
+		fmt.Printf("version:%s, kind: %s\n",version,kind)
+
+		b, err := data.MarshalJSON()
+		if err != nil {
+			fmt.Printf("MarshalJSON is failed")
+			break
+		}
+
+		obj, _, err := unstructured.UnstructuredJSONScheme.Decode(b, nil, nil)
+		if err != nil {
+			fmt.Printf("Decode is failed")
+			break
+		}
+
+		err = PrintObjectType(obj)
+		if err != nil {
+			fmt.Printf("PrintObjectType is failed")
+		}
+	}
 }
